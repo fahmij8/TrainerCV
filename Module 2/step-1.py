a8ed51c06577dff9887e1bf9a85f30a94962770f
Fahmi Jabbar <fahmijabbar@upi.edu>
2021-08-11 00:02:56 +0700

    Re-initialize Module 2

---
+++ Module 2/step-1.py
@@ -0,0 +1,44 @@
+# =========== Module 1, Step 1 : Dataset Taking (Your Face Sample) =========== #
+import cv2
+import utilities_modul as util
+
+if __name__ == '__main__':
+    # Initialize Directory
+    util.init_directory(1)
+
+    # Load HAAR face classifier
+    face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # Initialize Webcam
+    cap = util.init_camera(util.init_data("urlCamera"))
+    count = 0
+
+    # Collect 20 samples of your face from webcam input
+    print("[!] Taking samples") 
+    while True:
+        try:
+            ret, frame = cap.read()
+            if util.face_extractor(frame, face_classifier) is not None:
+                count += 1
+                face = cv2.resize(util.face_extractor(frame, face_classifier), (400, 400))
+
+                # Save file in specified directory with unique name
+                file_name_path = './dataset/myface/' + str(count) + '.jpg'
+                cv2.imwrite(file_name_path, face)
+
+                # Put count on images and display live count
+                cv2.putText(face, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+                cv2.imshow('Dataset Taker', face)
+                
+            else:
+                print("Face not found")
+                pass
+
+            if cv2.waitKey(1) == 13 or count == 20: 
+                break
+        except:
+            print("[!] Change your webcam URL if you see this many times.")
+            pass    
+
+    cv2.destroyAllWindows()      
+    print("[!] Collecting Samples Complete")
---
+++ Module 2/step-4.py
@@ -0,0 +1,73 @@
+# =========== Module 1, Step 3 : Model Testing =========== #
+import cv2, sys
+import tensorflow as tf
+import numpy as np
+import utilities_modul as util
+sys.path.append("/usr/grading")
+import grad
+from PIL import Image
+
+if __name__ == '__main__':
+    # Read Credential
+    usermail = util.init_data("email")
+    appName = util.init_data("appName")
+    deviceName = util.init_data("deviceName")
+    key = util.init_data("xm2morigin")
+
+    # Load Models
+    model = tf.keras.models.load_model('model_module-1.h5')
+
+    # Load HAAR face classifier
+    face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # Initialize Webcam
+    cap = util.init_camera(util.init_data("urlCamera"))
+    detectedTimes = 15
+    flagGrading = False
+
+    # Testing Model
+    while True:
+        try:
+            ret, frame = cap.read()
+            face=util.face_extractor_boundaries(frame, face_classifier)
+            # If Webcam detecting face
+            if type(face) is np.ndarray:
+                # Preprocess Image
+                face = cv2.resize(face, (224, 224))
+                im = Image.fromarray(face, 'RGB')
+                img_array = np.array(im)
+                img_array = np.expand_dims(img_array, axis=0)
+                pred = model.predict(img_array)
+                confidence = int(pred[0][0] * 100)
+                print(confidence)
+                # Image Labeling
+                # If prediction confidence > 0.5 your face is detected 
+                if(confidence > 50):
+                    detectedTimes -= 1 
+                    label = f"Face : ({confidence}%)"
+                    cv2.putText(frame,label, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+                    util.postRequest(1, appName, deviceName, key)
+                else:
+                    label = f"Emtpy : ({confidence}%)"
+                    cv2.putText(frame,label, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,255), 2)
+                    util.postRequest(0, appName, deviceName, key)
+
+                if(detectedTimes == 0 and flagGrading == False):
+                    cv2.destroyAllWindows()
+                    break
+            # Else, webcam not detecting any images
+            else:
+                cv2.putText(frame,"Empty", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+                flagGrading = False
+            
+            cv2.imshow('Video', frame)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        except:
+            flagGrading = None
+            print("[!] Change your webcam URL if you see this many times.")
+            pass
+
+    cv2.destroyAllWindows()
+    print("[!] Testing complete, Congratulations!")
